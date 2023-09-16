f1554c321db170e906697f29e2c86cea936e357f
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-09-16 18:41:50 -0500

    feat: :sparkles: Suspense

--- app/posts/[postId]/page.jsx
+++ app/posts/[postId]/page.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from "react";
+import Posts from "../page";
+import { Suspense } from "react";
 
 const loadPost = async (postId) => {
-  const rest = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`);
+  const rest = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${postId}`
+  );
   const data = rest.json();
   return data;
-}
+};
 
-
-async function page({params}) {
-
+async function page({ params }) {
   const post = await loadPost(params.postId);
 
   return (
@@ -16,8 +18,15 @@ async function page({params}) {
       <h1>Post Page: {post.id}</h1>
       <h2>{post.title}</h2>
       <p>{post.body}</p>
+      <hr />
+
+      <h3>Otras publicaciones</h3>
+
+      <Suspense fallback={<div>Cargando...</div>}>
+        <Posts />
+      </Suspense>
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
--- app/posts/page.jsx
+++ app/posts/page.jsx
@@ -5,7 +5,7 @@ const loadPosts = async () => {
   const rest = await fetch('https://jsonplaceholder.typicode.com/posts');
   const data = rest.json();
 
-  // await new Promise(resolve => setTimeout(resolve, 3000));
+  await new Promise(resolve => setTimeout(resolve, 3000));
 
   return data;
 }
