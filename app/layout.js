90ee935dcdac8d0de93eb5f8ed719f166d64a4fe
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-09-15 21:53:56 -0500

    feat: :sparkles: Fonts

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,24 @@
 import Navbar from "../components/Navbar"
+import { Roboto } from 'next/font/google'
+
 
 export const metadata = {
   title: 'Mi tiendita con Next.js - HomePage',
   description: 'Esta es la pagina de inicio de mi tiendita con Next.js',
   keywords: 'tienda, next.js, ecommerce, react, javascript',
-  
+
 }
  
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  styles: ['normal', 'italic'],
+  subsets: ['latin'],
+})
+
 export default function RootLayout({ children }) {
  return (
     <html lang="en">
-      <body>
+      <body className={roboto.className}>
         <Navbar/>
         
         {children}
