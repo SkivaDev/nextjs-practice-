af2d33c98dbaf019b96bc9dc4d1272f46950da18
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-09-16 18:09:30 -0500

    feat: :sparkles: Loading Page

---
+++ app/posts/loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function loading() {
+  return (
+    <div>Esta cargando pepepe</div>
+  )
+}
+
+export default loading
--- app/posts/page.jsx
+++ app/posts/page.jsx
@@ -4,6 +4,9 @@ import React from 'react'
 const loadPosts = async () => {
   const rest = await fetch('https://jsonplaceholder.typicode.com/posts');
   const data = rest.json();
+
+  // await new Promise(resolve => setTimeout(resolve, 3000));
+
   return data;
 }
 
