2e0af772d3228f099141bae0e209dd166bd6259a
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-09-16 19:09:55 -0500

    feat: :lipstick: Add Tailwind CSS

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import Navbar from "../components/Navbar"
 import { Roboto } from 'next/font/google'
-
+import './globals.css'
 
 export const metadata = {
   title: 'Mi tiendita con Next.js - HomePage',
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,8 +3,9 @@ import Link from "next/link"
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
+    <nav className="flex justify-between items-center p-[1.875rem] bg-indigo-400">
+      <h1 className="text-[2rem]">NextJS APP</h1>
+      <ul className="flex gap-3 px-[1.25rem]">
         <li>
           <Link href="/">Home</Link>
         </li>
