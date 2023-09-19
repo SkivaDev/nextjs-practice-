abdaeb2a2d82f03143944554419e67265a4d6bb7
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-09-19 16:30:25 -0500

    feat: :sparkles: SearchParams

--- src/app/api/users/[userId]/route.js
+++ src/app/api/users/[userId]/route.js
@@ -2,6 +2,11 @@ const { NextResponse } = require("next/server");
 
 export async function GET(request, { params }) {
 
+  const { searchParams } = new URL(request.url);
+  console.log(searchParams)
+  console.log(searchParams.get('name'))
+  console.log(searchParams.get('age'))
+
   const res = await fetch(`https://jsonplaceholder.typicode.com/users/${params.userId}`);
   const user = await res.json();
 
